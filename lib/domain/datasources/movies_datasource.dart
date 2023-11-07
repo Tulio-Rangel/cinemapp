a43033eb87704627522c2d66e591854621f15868
Tulio-Rangel <tjrn20@hotmail.com>
2023-11-07 18:19:18 -0500

    start datasource impl fot the moviedb

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,6 +1,6 @@
 // Esta clase define los metodos que voy a llamar para traer la data del domain/entity
 import 'package:cinemapp/domain/entities/movie.dart';
 
-abstract class MovieDatasource {
+abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
