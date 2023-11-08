757c4f164fb5f6ca49a7bcccb349ef64790a2bdc
Tulio-Rangel <tjrn20@hotmail.com>
2023-11-08 15:26:03 -0500

    create instance of repository with riverpod

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapp/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapp/infrastructure/repositories/movie_repository_impl.dart';
+
+// Este provider no se va a modificar asi que puede ser de solo lectura, es inmutable
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRespositoryImpl(MoviedbDatasource());
+});
