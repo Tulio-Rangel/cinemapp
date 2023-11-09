58ec2e2507b5afbc6a3eb846fa5137ded336f142
Tulio-Rangel <tjrn20@hotmail.com>
2023-11-09 16:54:33 -0500

    correct typo in url

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -5,7 +5,7 @@ class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
       adult: moviedb.adult,
       backdropPath: moviedb.backdropPath != ''
-          ? 'https//image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
           : 'https://studio.uxpincdn.com/studio/wp-content/uploads/2021/06/10-error-404-page-examples-for-UX-design.png.webp',
       genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
       id: moviedb.id,
