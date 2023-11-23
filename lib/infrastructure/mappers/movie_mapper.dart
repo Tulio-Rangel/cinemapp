c7647f035e4b79e57967781a352ba19c971a1b4c
Tulio-Rangel <tjrn20@hotmail.com>
2023-11-23 15:41:40 -0500

    change img when poster not found

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -8,7 +8,7 @@ class MovieMapper {
       adult: moviedb.adult,
       backdropPath: moviedb.backdropPath != ''
           ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-          : 'https://studio.uxpincdn.com/studio/wp-content/uploads/2021/06/10-error-404-page-examples-for-UX-design.png.webp',
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
       genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
       id: moviedb.id,
       originalLanguage: moviedb.originalLanguage,
@@ -17,7 +17,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: moviedb.posterPath != ''
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
