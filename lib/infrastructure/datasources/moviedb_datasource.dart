28e6ab1e5a4c24b5ed093a096c4a91d1ef8ea12a
Tulio-Rangel <tjrn20@hotmail.com>
2023-11-29 15:50:54 -0500

    create datasource & repository for searchMovies

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -83,4 +83,13 @@ class MoviedbDatasource extends MoviesDatasource {
 
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    //* Recibo la respuesta de la API
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+
+    return _jsonToMovies(response.data);
+  }
 }
