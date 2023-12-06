206fbe341b4ee6fda8c5f7e1e5677b0f0de0c330
Tulio-Rangel <tjrn20@hotmail.com>
2023-12-06 11:18:20 -0500

    correct typo

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -8,7 +8,7 @@ class CustomBottomNavigation extends StatelessWidget {
     return BottomNavigationBar(elevation: 0, items: const [
       BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
       BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: 'Categorias'),
+          icon: Icon(Icons.label_outline), label: 'Categorías'),
       BottomNavigationBarItem(
           icon: Icon(Icons.favorite_outline_outlined), label: 'Favoritos')
     ]);
