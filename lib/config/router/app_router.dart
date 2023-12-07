b6b2b43f07416cf69e31009b07e224b7d8de3e32
Tulio-Rangel <tjrn20@hotmail.com>
2023-12-06 20:20:05 -0500

    Thats how go router move in bottomNavBar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,11 +1,43 @@
 import 'package:cinemapp/presentation/screens/screens.dart';
+import 'package:cinemapp/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                  path: 'movie/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final movieId = state.params['id'] ?? 'no-id';
+                    return MovieScreen(movieId: movieId);
+                  }),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
+      ])
+
+  //* Rutas Padre/hija
+  /*
   GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen(
+            childView: HomeView(),
+          ),
       routes: [
         GoRoute(
             path: 'movie/:id',
@@ -15,4 +47,5 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
               return MovieScreen(movieId: movieId);
             }),
       ]),
+  */
 ]);
