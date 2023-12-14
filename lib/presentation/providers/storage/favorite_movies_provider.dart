7a1c59d15c1a1e8ed0ee0637d73615036b94101c
Tulio-Rangel <tjrn20@hotmail.com>
2023-12-14 14:39:10 -0500

    create favorite movies provider

---
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -0,0 +1,38 @@
+import 'package:cinemapp/domain/entities/movie.dart';
+import 'package:cinemapp/domain/repositories/local_storage_repository.dart';
+import 'package:cinemapp/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final favoriteMoviesProvider =
+    StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
+  final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+  return StorageMoviesNotifier(localStorageRepository: localStorageRepository);
+});
+
+/*
+  {
+    1234: Movie,
+    2345: Movie2,
+    1532: Movie3
+  }
+*/
+
+class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+
+  StorageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final movies = await localStorageRepository.loadMovies(
+        offset: page * 10); // TODO: Limit 20
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+    for (final movie in movies) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMoviesMap};
+  }
+}
