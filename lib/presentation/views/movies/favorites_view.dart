f8d132514c8743075c1fecc02b1dacaac43a1f1d
Tulio-Rangel <tjrn20@hotmail.com>
2023-12-15 16:31:58 -0500

    Show Favorites Movies Title

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,18 +1,38 @@
+import 'package:cinemapp/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref
+        .watch(favoriteMoviesProvider)
+        .values
+        .toList(); //* Convierto el mapa en una lista
+
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Favorites View'),
-      ),
-      body: const Center(
-        child: Text('Favoritos'),
-      ),
+      body: ListView.builder(
+          itemCount: favoritesMovies.length,
+          itemBuilder: ((context, index) {
+            final movie = favoritesMovies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          })),
     );
   }
 }
