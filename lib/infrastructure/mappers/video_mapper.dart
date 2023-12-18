95878e32e2c13f954817ce8259d683ea3d20ef6f
Tulio-Rangel <tjrn20@hotmail.com>
2023-12-18 17:49:25 -0500

    create video mapper

---
+++ lib/infrastructure/mappers/video_mapper.dart
@@ -0,0 +1,10 @@
+import 'package:cinemapp/domain/entities/video.dart';
+import '../models/moviedb/moviedb_videos.dart';
+
+class VideoMapper {
+  static moviedbVideoToEntity(Result moviedbVideo) => Video(
+      id: moviedbVideo.id,
+      name: moviedbVideo.name,
+      youtubeKey: moviedbVideo.key,
+      publishedAt: moviedbVideo.publishedAt);
+}
